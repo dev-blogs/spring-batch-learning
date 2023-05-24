393e8ef2705aada581c4daf1a6a4d931093d1102
zheka <zheka0783@yandex.ru>
2023-05-24 19:55:36 +0300

    Code refactoring. Changed SingleConnectionDataSource data source by MysqlDataSource

--- src/main/java/com/example/batch/config/DatabaseConfig.java
+++ src/main/java/com/example/batch/config/DatabaseConfig.java
@@ -1,41 +1,46 @@
 package com.example.batch.config;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
 @PropertySource("classpath:src/main/resources/application.properties")
 public class DatabaseConfig {
 
-    @Value("${url}")
-    private String url;
+    @Value("${databaseName}")
+    private String databaseName;
+    @Value("${host}")
+    private String host;
     @Value("${login}")
     private String login;
     @Value("${password}")
     private String password;
     @Bean
-    public DataSource dataSource() {
-        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(login);
+    public DataSource dataSource() throws SQLException {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setDatabaseName(databaseName);
+        dataSource.setServerName(host);
+        dataSource.setPort(3306);
+        dataSource.setUser(login);
         dataSource.setPassword(password);
-        dataSource.setSuppressClose(true);
+        dataSource.setServerTimezone("UTC");
         return dataSource;
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
+    public JdbcTemplate jdbcTemplate() throws SQLException {
         return new JdbcTemplate(dataSource());
     }
 
     @Bean
-    public DataSourceTransactionManager transactionManager() {
+    public DataSourceTransactionManager transactionManager() throws SQLException {
         return new DataSourceTransactionManager(dataSource());
     }
 }
