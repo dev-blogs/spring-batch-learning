829ca10094ade48a69dac8f1db6a38391060aa37
zheka <zheka0783@yandex.ru>
2024-01-02 15:14:47 +0200

    4-1-2 Code development. Added ThreadPoolTaskExecutor class for concurrent execution of jobs

--- src/test/java/com/example/ProductStepTest.java
+++ src/test/java/com/example/ProductStepTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -36,8 +37,11 @@ public class ProductStepTest {
 				.addString("testdata", "test")
 				.addLong("timestamp", System.currentTimeMillis())
 				.toJobParameters();
-		
-		jobLauncher.run(job, jobParameters);
+
+		JobExecution jobExecution = jobLauncher.run(job, jobParameters);
+		System.out.println(jobExecution.getStatus());
+
+		Thread.sleep(1000);
 		
 		assertEquals(5, simpleJdbcTemplate.queryForInt("SELECT count(*) FROM products"));
 	}
