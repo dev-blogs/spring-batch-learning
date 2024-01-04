20b65a727459cdb53792c473bca8bd5c41bd7d87
zheka <zheka0783@yandex.ru>
2024-01-04 17:37:32 +0200

    4-1-2 Code development. Added concurrent execution
    + Added ThreadPoolTaskExecutor class for concurrent execution
    + Added DataSourceInitializer bean

--- src/main/java/com/example/batch/config/InfrastructureConfig.java
+++ src/main/java/com/example/batch/config/InfrastructureConfig.java
@@ -4,26 +4,51 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import javax.sql.DataSource;
 
 @Configuration
 public class InfrastructureConfig {
+    @Value("classpath:db/create-tables.sql")
+    private Resource schemaScript;
+
+    @Value("classpath:org/springframework/batch/core/schema-h2.sql")
+    private Resource dataScript;
+
     @Bean
     public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("db/create-tables.sql")
-                .addScript("org/springframework/batch/core/schema-h2.sql")
-                .build();
-        return db;
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setUrl("jdbc:h2:mem:testDb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populateDb());
+        return initializer;
+    }
+
+    private DatabasePopulator populateDb() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schemaScript);
+        populator.addScript(dataScript);
+        return populator;
     }
 
     @Bean
@@ -46,9 +71,17 @@ public class InfrastructureConfig {
     }
 
     @Bean
+    public TaskExecutor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(10);
+        return executor;
+    }
+
+    @Bean
     public JobLauncher jobLauncher() throws Exception {
         SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
         jobLauncher.setJobRepository(jobRepository());
+        jobLauncher.setTaskExecutor(executor());
         return jobLauncher;
     }
 }
